4c41f7c97142f39c61c9464c89bb1cb22efbc42f
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-10-27 20:19:52 -0400

    Création du TP
    Création du projet de base avec SDL2 pour le worms

---
+++ TP_Codename_WORMS/TP_Codename_WORMS/main.cpp
@@ -0,0 +1,16 @@
+/*
+420-203-RE : Atelier : Image clickable
+Crée le 9 novembre 2014 par Kevin Pantelakis( DA : 1303126  /  E-mail : kev.pantelakis@gail.com)
+*/
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_image.h>
+#include <SDL2\SDL_ttf.h>
+#include <SDL2\SDL_mixer.h>
+
+int main(int argc, char** argv) {
+	return 0;
+}
