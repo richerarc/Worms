698ed053f46a0d1d6300ed32af8fcac028e6858b
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-10-28 21:23:29 -0400

    Continuation du code pour CMenus

--- TP_Codename_WORMS/TP_Codename_WORMS/CMenu.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CMenu.h
@@ -12,13 +12,24 @@ public:
 	@ Constructeur
 	@brief Initialise les données membres
 	@param _Window : Fenêtre ou afficher le renderer
-	@return l'adresse mémoire de l'objet 
+	@return l'adresse mémoire de l'objet
 	*/
 	CMenu(SDL_Window* _Window){
 		m_pList = new CListeDC < CGUIE* >;
 		SDL_CreateRenderer(_Window, NULL, NULL);
 	}
 
+	/*!
+	@Method AddElement
+	@brief Ajoute un Contrôle visuel dans une liste.
+	@param  _Element: Contrôle visuel à ajouter
+	@param  _uiX: Position en x
+	@param  _uiY: Position en y
+	@param  _uiW: Largueur du contrôle
+	@param  _uiH: Hauteur du contrôle
+	@return true si il n'y avait aucun doublon et ajoute l'objet
+	@return false si il y un doublon et ne l'ajoute pas à la liste.
+	*/
 	bool AddElement(CGUIE* _Element, unsigned int _uiX, unsigned int _uiY, unsigned int _uiW, unsigned int _uiH){
 		for (int i = 0; i < m_pList->Count(); i++)
 		{
@@ -26,10 +37,35 @@ public:
 				return false;
 			m_pList->AllerSuivant();
 		}
+		_Element->setPos(_uiX, _uiY);
+		_Element->setSize(_uiW, _uiH);
 		m_pList->AjouterFin(_Element);
 		return true;
 	}
 
+	/*!
+	@Method Render
+	@brief Appelle la fonction Draw de chaque contrôle visuel dans la liste.
+	@param  Aucun
+	@return false si il y un doublon et ne l'ajoute pas à la liste.
+	*/
+	void Render(){
+		m_pList->AllerDebut();
+		for (int i = 0; i < m_pList->Count(); i++)
+		{
+			m_pList->ObtenirElement()->Draw(m_pRenderer);
+			m_pList->AllerSuivant();
+		}
+	}
+
+	/*!
+	@Method setOnCreenPos
+	@brief Définit un nouvel emplacement pour le contrôle visuel.
+	@param  _Element: Contrôle visuel à ajouter
+	@param  _uiX: Position en x
+	@param  _uiY: Position en y
+	@return Aucun.
+	*/
 	void setOnScreenPos(const char* _Name, unsigned int _uiX, unsigned int _uiY){
 		m_pList->AllerDebut();
 		for (int i = 0; i < m_pList->Count(); i++)
@@ -42,15 +78,28 @@ public:
 		}
 	}
 
+	/*!
+	@Method setSize
+	@brief Modifie la taille du contrôle visuel.
+	@param  _Name: Nom du controle a changer.
+	@param  _uiWidth: Largueur.
+	@param  _uiHeight: Hauteur.
+	@return Aucun.
+	*/
 	void setSize(const char* _Name, unsigned int _uiWidth, unsigned int _uiHeight){
 		m_pList->AllerDebut();
 		for (int i = 0; i < m_pList->Count(); i++)
 		{
 			if (m_pList->ObtenirElement()->getName() == _Name){
-				m_pList->ObtenirElement()->setPos(_uiWidth,_uiHeight);
+				m_pList->ObtenirElement()->setPos(_uiWidth, _uiHeight);
 				break;
 			}
 			m_pList->AllerSuivant();
 		}
 	}
+
+	SDL_Renderer* getRenderer(){
+		return m_pRenderer;
+	}
+
 };
