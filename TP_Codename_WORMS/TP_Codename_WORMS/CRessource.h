9aa68c2fd01378a0881bf6ff4678154d988dc521
DomFC <dominiquefauteuxchapleau@hotmail.com>
2014-11-04 09:34:20 -0500

    Ajout de GetName dans CRessource et mise à jour du JDB

--- TP_Codename_WORMS/TP_Codename_WORMS/CRessource.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CRessource.h
@@ -11,16 +11,30 @@
 #ifndef TP_Codename_WORMS_CRessource_h
 #define TP_Codename_WORMS_CRessource_h
 
+
+/*
+@class CRessource
+@discussion Classe représentant une ressource du gestionnaire
+*/
+
 class CRessource {
 private:
 	const char* m_strName;
 	
 public:
+	//Constructeur...
 	CRessource(const char* _Name){
 		m_strName = _Name;
 	}
+
+	//Destructeur...
 	~CRessource(){
 		
+	}
+
+	//Accesseur...
+	const char* GetName(){
+		return m_strName;
 	}
 };
 
