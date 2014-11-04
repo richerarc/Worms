818909798cd77281473ca4afd868615db4328d10
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-11-04 17:58:40 -0500

    Continuation du code de MenuDeclaration

--- TP_Codename_WORMS/TP_Codename_WORMS/MenuDeclaration.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/MenuDeclaration.h
@@ -15,18 +15,32 @@ void LoadResources(){
 	GestionnaireDeResources->Ajouter(new CSprite("SpriteBtnClick", IMG_LoadTexture(Window->getRenderer(), "C:\\Users\\1303126\\Documents\\GitHub\\Worms\\Ressources\\Btn1.png"), 2, 4, 10, 0));
 }
 
+
+
 /*!
  @discussion Déclaration du Menu Principal.
  */
-CMenu* MenuPrincipal = new CMenu(Window->getRenderer(), {0,0,WIDTH,HEIGHT});
-CButton* ButtonNewGame;
-CButton* ButtonQuit;
+CMenu* MenuPrincipal;
+CButton* ButtonNewGame = new CButton("BtnNewGame", "Play", (CFont*)GestionnaireDeResources->Obtenir("Font"), {0,0,0,0}, (CSprite*)GestionnaireDeResources->Obtenir("SpriteBtnClick"));
+CButton* ButtonQuit = new CButton("BtnQuit", "Quit", (CFont*)GestionnaireDeResources->Obtenir("Font"), { 0, 0, 0, 0 }, (CSprite*)GestionnaireDeResources->Obtenir("SpriteBtnClick"));
 
-
-
-
-
-
+/*!
+@discussion Déclaration du Menu nouvelle partie.
+*/
 CMenu* MenuNewGame;
+
+/*!
+@discussion Déclaration du Menu Créarion d'une nouvelle équipe.
+*/
 CMenu* MenuNewTeam;
+
+/*!
+@discussion Déclaration du Menu pause.
+*/
 CMenu* MenuPause;
+
+void CreateMenuPrincipal(){
+	MenuPrincipal = new CMenu(Window->getRenderer(), { 0, 0, WIDTH, HEIGHT });
+	MenuPrincipal->AddElement(ButtonNewGame, WIDTH - 200, HEIGHT - 130, 100, 30);
+	MenuPrincipal->AddElement(ButtonQuit, WIDTH - 200, HEIGHT - 170, 100, 30);
+}
