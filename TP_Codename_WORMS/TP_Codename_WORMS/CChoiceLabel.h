98dcdd37391223964d0c2fbfd7ff28f269067366
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-11-04 08:33:26 -0500

    Continuation de la classe CChoice label

--- TP_Codename_WORMS/TP_Codename_WORMS/CChoiceLabel.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CChoiceLabel.h
@@ -1,11 +1,12 @@
-class  CChoiceLabel : CGUIE{
+class  CChoiceLabel :public CGUIE{
 private:
 	CButton* PrevButton;
 	CButton* NextButton;
 	CLabelImage* LabelMap;
 public:
-	 CChoiceLabel();
-	~ CChoiceLabel();
+	CChoiceLabel(const char* _Name, const char* _strText, CFont* _Font, SDL_Rect _Rect, CButton* _PrevBtn , CButton* _Nextbtn, CLabel* _LabelMap) :CGUIE(){}
+
+	~CChoiceLabel(){}
 
 
 };
