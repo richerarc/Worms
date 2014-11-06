a910cef8e4ca7775cebda9b0328769702e314584
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-11-06 14:26:26 -0500

    Création de méthodes dans CGestionnaire

--- TP_Codename_WORMS/TP_Codename_WORMS/CSurface.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CSurface.h
@@ -1,1 +1,16 @@
-class CSurface :public CRessource{};
+class CSurface :public CRessource{
+public:
+	SDL_Surface* m_pSurface;
+private:
+
+	CSurface(const char* _strName, SDL_Surface* _Surface):CRessource(_strName){
+		m_pSurface = _Surface;
+	}
+
+	~CSurface(){}
+
+	SDL_Surface* getSurface(){
+		return m_pSurface;
+	}
+
+};
