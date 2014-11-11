1c99cc03753a5f31b3e80bc50fd2094b7ff19ccf
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-11-11 14:17:48 -0500

    Création de la classe CEntity et CTeam

--- TP_Codename_WORMS/TP_Codename_WORMS/CEntity.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CEntity.h
@@ -3,32 +3,63 @@
 
 
 /*!
-@CGrenades
-@Classe permettant la création d'une grenade
+@CEntity
+@Classe Abstraite représentant les entitées affectés par la physique.
 */
 class CEntity{
+private:
+	const char* m_strName;
+	unsigned int m_uiMasse;
+	SDL_Rect m_RectPosition;
+	bool m_boFocus;
 
 public:
+
 	/*!
 	@Constructeur
 	@Description: Permet d'initialiser les données membres
-	@Paramètres: Le nom du labelImage afin de l'identifier, le text à l'intérieur, le font et le rect, et la texture contenant l'image.
-	@Classe héritant de CGUIE, donc elle passe les paramètres nécessaires au constructeur de CGUIE
+	@Paramètres:
 	*/
-	CEntity(){
+	CEntity(const char* _Name, unsigned int _uiMasse,SDL_Rect _RectPos){
+		m_strName = _Name;
+		m_uiMasse = _uiMasse;
+		m_RectPosition = _RectPos;
+		m_boFocus = false;
 	}
+
 	/*!
 	@Destructeur:
 	@Permet de détruire les objets créés en mémoire
 	*/
 	~CEntity(){
+		delete m_strName;
 	}
-
-
 
 	/*!
 	@Accesseurs:
 	*/
+	const char* getName(){
+		return m_strName;
+	}
+
+	unsigned int m_uiMasse(){
+		return m_uiMasse;
+	}
+
+	SDL_Rect getPosition(){
+		return m_RectPosition;
+	}
+
+	bool isFocused(){
+		return m_boFocus;
+	}
+
+	void setFocus(bool _Focus){
+		m_boFocus = _Focus;
+	}
+
+	virtual void ReactToExplosion() = 0;
+	virtual void HandleEvent() = 0;
 
 };
 
---
+++ TP_Codename_WORMS/TP_Codename_WORMS/CTeam.h
@@ -0,0 +1,17 @@
+//
+//  CTeam.h
+//  
+//  Created by Kevin Pantelakis on 2014-11-11.
+//
+
+/*!
+@class CTeam
+@discussion Classe représentant une équipe.
+*/
+class CTeam{
+private:
+	CListeDC<CWorm*>* m_pListeWorm;
+
+
+
+};
