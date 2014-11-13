9ccdc7d9cdec39fc2c0d545600e14622db48003d
Diego <dipavit@hotmail.com>
2014-11-13 16:48:54 -0500

    Créations de la classe CPhysics

---
+++ TP_Codename_WORMS/TP_Codename_WORMS/CPhysic.h
@@ -0,0 +1,41 @@
+#include "C2DVector.h"
+//#include <time.h>
+
+class CPhysics{
+private:
+	C2DVector * m_Wind;
+	double m_Gravity;
+	double m_MaxSpeed;
+	double m_MaxWindSpeed;
+	//srand(time(NULL));
+
+public:
+	// Constructeur:
+	//	Parametres: _Gravity - affecte la valeur de la gravité (1 fois par jeu).
+	//				_MaxSpeed - affecte la valeur maximale de l'accélération gravitationnelle.
+	//				_MaxWindSpeed - affecte la valeur maximale de la force du vent sur un axe (donc maximum: MaxWindSpeed sur X + MaxWindSpeed sur Y).
+	CPhysics(){
+		m_Wind->setX(0);
+		m_Wind->setY(0);
+		m_Gravity = 1;
+		m_MaxSpeed = 1;
+		m_MaxWindSpeed = 50;
+	}
+	
+	//Méthode: RedefineWind permet de changer aleatoirement la direction du vent.
+	void RedefineWind(){
+		m_Wind->setX(rand() % (m_MaxWindSpeed / 2) - (MaxWindSpeed / 4));
+		m_Wind->setY(rand() % (m_MaxWindSpeed / 2) - (MaxWindSpeed / 4));
+	}
+
+	//Méthode: VerifyCollision permet de vérifier une collision entre deux entités
+	//		Paramètres:
+	bool VerifyCollision(CEntity* _Collider, CEntity* _Collidee){
+
+	}
+
+
+
+
+
+};
