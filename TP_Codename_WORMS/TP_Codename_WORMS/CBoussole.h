fa0b564dd508e99dd64e53b4912aea1796ec51a3
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-11-14 15:11:59 -0500

    Ajout de CBoussole  et des images associées

---
+++ TP_Codename_WORMS/TP_Codename_WORMS/CBoussole.h
@@ -0,0 +1,14 @@
+//
+//  CBoussole.h
+//  TP_Codename_WORMS
+//
+//  Créé par Kevin Pantelakis le 2014-10-28.
+//  Mise à jour par Kevin Pantelakis le 2014-10-29
+//  
+//
+
+/*!
+@class CBoussole.h
+@discussion Classe qui représente la boussole.
+*/
+class CBoussole{};
--- TP_Codename_WORMS/TP_Codename_WORMS/CGame.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CGame.h
@@ -4,7 +4,15 @@
 //  Created by Kevin Pantelakis on 2014-11-11.
 //
 class CGame{
+private:
 
+	CListeDC<CTeam*>* m_pListeTeam;
+	CMap* m_pMap;
 
+public:
+
+	CGame(){}
+
+	~CGame(){}
 
 };
