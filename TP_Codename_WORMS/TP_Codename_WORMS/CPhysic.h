4053072d0011fef9f1f5e863f3a9369f5ac49062
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-12-18 07:52:04 -0500

    ya dequoi qqui me gossait dans les commentaires

--- TP_Codename_WORMS/TP_Codename_WORMS/CPhysic.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CPhysic.h
@@ -21,7 +21,7 @@
 class CPhysics{
 private:
 	static C2DVector * m_Wind;			// Le vent
-	static double m_Gravity;			// La gravit�
+	static double m_Gravity;			// La gravité
 	static int m_MaxSpeed;				// La vitesse maximum
 	static int m_MaxWindSpeed;			// Le vent maximum
 	static SDL_Surface * m_Map;			// Le Cham de battaile ou la physique aura son effet.
@@ -32,12 +32,12 @@ public:
 	
 	/*!
 	 @method Init.
-	 @brief Initialise les donn�es membres.
+	 @brief Initialise les données membres.
 	 @param _map: Champ de battaile
-	 @param _gravity: Gravit� qui influra sur les entit�es
+	 @param _gravity: Gravité qui influra sur les entitées
 	 @param _maxWind: Vent maximum.
-	 @return Adresse m�moire de l'objet.
-	 @discussion Comme tous les donn�es membres sont statiques, Le init agit comme un constructeur.
+	 @return Adresse mémoire de l'objet.
+	 @discussion Comme tous les données membres sont statiques, Le init agit comme un constructeur.
 	 */
 
 	static void Init(SDL_Surface* _map, double _gravity, int _maxWind){
@@ -79,7 +79,7 @@ public:
 	
 	/*!
 	 @method Verify collision
-	 @brief V�rifie si deux rect se touche
+	 @brief Vérifie si deux rect se touche
 	 @param _Collider: Le rect en mouvement
 	 @param _Collidee: Le rect immobile .
 	 @return true si il y a un collision?
@@ -132,13 +132,13 @@ public:
 	
 	/*!
 	 @method VerifyGroudColision
-	 @brief V�rifie si deux rect se touche
+	 @brief Vérifie si deux rect se touche
 	 @param _Collider: Le rect en mouvement
 	 @param _Collidee: Le rect immobile .
-	 @return NOCONTACT : Le rectangle ne touche � rien
+	 @return NOCONTACT : Le rectangle ne touche é rien
 	 @return GROUND : Le rectangle touche au sol
-	 @return LEFT : Le rectangle touche le terrain � gauche
-	 @return RIGHT : Le rectangle touche le terrain � droite
+	 @return LEFT : Le rectangle touche le terrain é gauche
+	 @return RIGHT : Le rectangle touche le terrain é droite
 	 @return CEILING : Le rectangle touche le plafond
 	 @return GROUNDLEFT : Le rectangle touche le sol et la gauche
 	 @return GROUNDRIGHT : Le rectangle touche le sol et la droite
@@ -160,12 +160,12 @@ public:
 	
 	/*!
 	 @method VerifyNextPosition
-	 @brief V�rifie la position prochaine pour les collisions.
+	 @brief Vérifie la position prochaine pour les collisions.
 	 @param _Trajectoire: Trajectoire a parcourir par l'objet.
 	 @param _EntityRect: Rect de l'objet qui tombe
 	 @return La position de la collision si il y en a une.
 	 @return La position finale si la trajectoire c'est fait sans collisions.
-	 @return nullptr si le d�placement n'est pas significatif.
+	 @return nullptr si le déplacement n'est pas significatif.
 	 @discussion Retourne toujours une position valide sauf si le vecteur Original n'est pas assez gros.
 	 */
 	static CPosition* VerifyNextPosition(CTrajectory* _Trajectoire, SDL_Rect _EntityRect){
@@ -416,10 +416,10 @@ public:
 	
 	/*!
 	 @method Evaluate Slope
-	 @brief  Fonction qui retourne la pente (en degr�s) � partir d'une section de la map
-	 @param _Pos : Position dans la surface o� la pente est �valu�e
-	 @param _Direction : Direction de laquelle vient l'entit� impliqu�e
-	 @return L'angle de la pennte en degr�;
+	 @brief  Fonction qui retourne la pente (en degrés) é partir d'une section de la map
+	 @param _Pos : Position dans la surface oé la pente est évaluée
+	 @param _Direction : Direction de laquelle vient l'entité impliquée
+	 @return L'angle de la pennte en degré;
 	 */
 	static double EvaluateSlope(SDL_Rect _Rect){
 		double Slope = 0;
@@ -451,7 +451,7 @@ public:
 	 Method : VerifyIfSliding
 	 Brief : Fonction qui retourne la trajectoire d'une chose en train de glisser par rapport au terrain
 	 Params:
-	 _Rect : Rectangle � �valuer
+	 _Rect : Rectangle é évaluer
 	 */
 	static bool VerifyIfSliding(SDL_Rect* _Rect){
 		int iNbrPixels = 0; //Nombre de pixels en contact directement sous le rectangle
@@ -460,15 +460,15 @@ public:
 				iNbrPixels++;
 			}
 		}
-		return (iNbrPixels < _Rect->w / 2);//On suppose qu'� 50% du rect en contact avec le sol, aucune glissade ne survient
+		return (iNbrPixels < _Rect->w / 2);//On suppose qu'é 50% du rect en contact avec le sol, aucune glissade ne survient
 	}
 	
 	/*!
 	 @method Propulsion
-	 @brief  Retourne une trajectoire selon un vitesse de d�part et un acc�l�ration
+	 @brief  Retourne une trajectoire selon un vitesse de départ et un accélération
 	 @param _PosInit: Position initiale de l'objet subbisant la propulsion
 	 @param _Vit: Vitesse et direction initial du glissement
-	 @param _Acc: Position initial de l'entit�
+	 @param _Acc: Position initial de l'entité
 	 @return La trajectoire de ala propuslion
 	 @discussion Aucune.
 	 */
@@ -479,7 +479,7 @@ public:
 	/*!
 	 @method Slide
 	 @brief  Fonction qui fait glisser si la pente est trop grande
-	 @param _Rect : Rectangle de l'objet impliqu�
+	 @param _Rect : Rectangle de l'objet impliqué
 	 @return La trajectoire du glissement
 	 */
 	/*static CTrajectory * Slide(CEntity* _Entity){
@@ -494,12 +494,12 @@ public:
 	
 	/*!
 	 @method Move
-	 @brief  Fonction qui ajuste la position suite � un mouvement sans acc�l�ration
-	 @param _Rect : Rectangle se d�pla�ant
-	 @param _Direction : Bool de la direction emprunt�e (true = gauche, false = droite))
-	 @return BLOCKED: Si l'entit� est pogn�e
-	 @return MOVING: Si l'entit� peut continuer
-	 @discussion Ne d�place actuellement que d'un pixel sur l'axe X, � changer si voulu La hauteur (en pixels) d'une pente "escaladable" en y sera � d�terminer (actuellement 3 pixels)
+	 @brief  Fonction qui ajuste la position suite é un mouvement sans accélération
+	 @param _Rect : Rectangle se déplaéant
+	 @param _Direction : Bool de la direction empruntée (true = gauche, false = droite))
+	 @return BLOCKED: Si l'entité est pognée
+	 @return MOVING: Si l'entité peut continuer
+	 @discussion Ne déplace actuellement que d'un pixel sur l'axe X, é changer si voulu La hauteur (en pixels) d'une pente "escaladable" en y sera é déterminer (actuellement 3 pixels)
 	 */
 	static int Move(SDL_Rect* _Rect, int _Direction){
 		if (_Direction == LEFT){
@@ -531,7 +531,7 @@ public:
 	
 	/*!
 	 @method Acesseurs
-	 @brief Servent a acceder/modifier aux donn�es membres.
+	 @brief Servent a acceder/modifier aux données membres.
 	 */
 	static C2DVector* GetWind(){
 		return m_Wind;
