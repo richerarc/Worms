9d724637051df739167e8033e901a4c7b7daadd8
Kevin Pantelakis <kev.pantelakis@gmail.com>
2014-12-18 17:19:25 -0500

    Tests

--- TP_Codename_WORMS/TP_Codename_WORMS/CPhysic.h
+++ TP_Codename_WORMS/TP_Codename_WORMS/CPhysic.h
@@ -98,6 +98,42 @@ public:
 
 
 
+
+	static void HandleGroundCollisionTest(SDL_Rect* _Rect, int _Direction,double _Angle){
+		if (_Angle != 0){
+			switch (_Direction) {
+			case UP_DOWNLEFT:
+			case DOWN_UPRIGHT:
+			case CEILINGLEFT:
+			case GROUNDRIGHT:
+				for (int i = 0; i < _Rect->w; i++){
+					while (((unsigned int*)m_Map->pixels)[(m_Map->w * (_Rect->y + _Rect->h)) + _Rect->x + i] != TRANSPARENCY){
+						_Rect->y--;
+					}
+					while (((unsigned int*)m_Map->pixels)[(m_Map->w * (_Rect->y + _Rect->h)) + _Rect->x + i] == TRANSPARENCY){
+						_Rect->y++;
+					}
+				}
+				break;
+			case DOWN_UPLEFT:
+			case UP_DOWNRIGHT:
+			case GROUNDLEFT:
+			case CEILINGRIGHT:
+				for (int i = _Rect->w; i > 0; i--){
+					while (((unsigned int*)m_Map->pixels)[(m_Map->w * (_Rect->y + _Rect->h)) + _Rect->x + i] != TRANSPARENCY){
+						_Rect->y--;
+					}
+					while (((unsigned int*)m_Map->pixels)[(m_Map->w * (_Rect->y + _Rect->h)) + _Rect->x + i] == TRANSPARENCY){
+						_Rect->y++;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+
+
 	static void HandleGroundCollision(SDL_Rect* _Rect, int _Direction){
 		switch (_Direction) {
 		case UP_DOWNLEFT:
@@ -435,7 +471,7 @@ public:
 			}
 		}
 		if (!(PointsSignificatifs - 1))
-			return NOANGLE;
+			return DegToRad(NOANGLE);
 		double Angle = atan(Slope / (PointsSignificatifs - 1));
 		return Angle;
 	}
@@ -484,6 +520,8 @@ public:
 		else{
 		}
 		}*/
+
+
 
 	/*!
 	 @method Move
